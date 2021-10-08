5e8ce14d5816ce5615b68d6b47e3d6b50e691a15
Nicolas <inovasong@me.com>
2021-10-08 12:55:33 +0200

    Add Scenes and test it

--- Code/Main.lua
+++ Code/Main.lua
@@ -1,8 +1,22 @@
+require("Libraries.ScenesController")
+
 function love.load()
+    love.window.setMode(1600,900)
+    changeScene("menu")
 end
 
 function love.update(dt)
+    updateCurrentScene(dt)
 end
 
 function love.draw()
+    drawCurrentScene(dt)
+end
+
+function love.mousepressed(pX, pY, pBtn)
+    mousePressed(pX, pY, pBtn)
+end
+
+function love.keypressed(pKey)
+    keyPressed(pKey)
 end
---
+++ Code/Scenes/GameScene.lua
@@ -0,0 +1,1 @@
+local scene = newScene("game")
