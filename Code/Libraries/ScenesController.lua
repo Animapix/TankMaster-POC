6b8d62ce6961b9675946e6f40db53b01cb326408
Nicolas <inovasong@me.com>
2021-10-08 12:40:31 +0200

    Add Scenes controller
    Scene management module and basic scene creation function

---
+++ Code/Libraries/ScenesController.lua
@@ -0,0 +1,48 @@
+local scenes = {}
+local currentScene = nil
+
+changeScene = function(pSceneLabel)
+    if currentScene ~= nil then currentScene.unload() end
+    currentScene = scenes[pSceneLabel]
+    if currentScene ~= nil then currentScene.load() end
+end
+
+updateCurrentScene = function(dt)
+    if currentScene ~= nil then currentScene.update(dt) end
+end
+
+drawCurrentScene = function(dt)
+    if currentScene ~= nil then currentScene.draw(dt) end
+end
+
+mousePressed = function(pX,pY,pBtn)
+    if currentScene ~= nil then currentScene.mousePressed(pX,pY,pBtn) end
+end
+
+keyPressed = function(pKey)
+    if currentScene ~= nil then currentScene.keyPressed(pKey) end
+end
+
+newScene = function(pLabel)
+    local scene = {}
+
+    scene.load = function()
+    end
+
+    scene.update = function(dt)
+    end
+
+    scene.draw = function()
+    end
+
+    scene.mousePressed = function(pX,pY,pBtn)
+    end
+
+    scene.keyPressed = function(pKey)
+    end
+
+    scene.unload = function()
+    end
+
+    scenes[pLabel] = scene
+end
